a41ff1ede8dd31be10d4b10ca684d65a1b3f99e0
Dev Jana <devjana@Devs-MBP-2.domain>
2020-09-09 11:41:57 -0500

    get working full stack

--- server/server.js
+++ server/server.js
@@ -11,6 +11,17 @@ app.use( bodyParser.urlencoded( { extended: true } ) );
 // globals
 const port = 3000;
 
+// db setup
+const Pool = pg.Pool;
+// configure the connection to db
+const pool = new Pool({
+    database: "music_library", // db name (NOT table name)
+    host: "localhost", // deafult when running locally, will change when deploying
+    port: 5432, // default port for local, also will change when deployed
+    max: 12, // max # of connections
+    idleTimeoutMillis: 20000 // connection time out in ms
+}); // end pool setup
+
 // spin up server
 app.listen( port, ()=>{
     console.log( 'server up:', port );
@@ -18,7 +29,15 @@ app.listen( port, ()=>{
 
 app.get( '/songs', ( req, res )=>{
     console.log( 'in /songs GET' );
-    res.send( 'ribbet' );
+    // test query: top 40 songs by rank
+    // SELECT * FROM "songs" ORDER BY "rank" DESC LIMIT 40;
+    const queryString = 'SELECT * FROM "songs" ORDER BY "rank" DESC LIMIT 40;';
+    pool.query( queryString ).then( ( results )=>{
+        res.send( results.rows ); //runs if query was successful
+    }).catch( ( err )=>{
+        console.log( err ); // if there was an error running query
+        res.sendStatus( 500 );
+    }) // end query
 }) // end /songs GET
 
 app.post( '/songs', ( req, res )=>{
